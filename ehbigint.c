cb2b3c2a554181165caface7e163ea75279df331
Eric Herman <eric@freesa.org>
2015-09-06 10:23:38 +0200

    just hack in some error messages

--- ehbigint.c
+++ ehbigint.c
@@ -2,9 +2,31 @@
 
 #include <stdio.h>
 
+#ifndef LOG_ERROR0
+#define LOG_ERROR0(format) \
+ fprintf(stderr, "Line %d: ", __LINE__); \
+ fprintf(stderr, format); \
+ fprintf(stderr, "\n")
+#endif
+
+#ifndef LOG_ERROR1
+#define LOG_ERROR1(format, arg) \
+ fprintf(stderr, "Line %d: ", __LINE__); \
+ fprintf(stderr, format, arg); \
+ fprintf(stderr, "\n")
+#endif
+
+#ifndef LOG_ERROR2
+#define LOG_ERROR2(format, arg1, arg2) \
+ fprintf(stderr, "Line %d: ", __LINE__); \
+ fprintf(stderr, format, arg1, arg2); \
+ fprintf(stderr, "\n")
+#endif
+
 static int nibble_to_hex(unsigned char nibble, char *c)
 {
 	if (c == 0) {
+		LOG_ERROR0("Null char pointer");
 		return 1;
 	}
 	if (nibble < 10) {
@@ -12,6 +34,7 @@ static int nibble_to_hex(unsigned char nibble, char *c)
 	} else if (nibble < 16) {
 		*c = 'A' + nibble - 10;
 	} else {
+		LOG_ERROR1("Bad input '%x'", nibble);
 		return 2;
 	}
 	return 0;
@@ -32,6 +55,7 @@ static int from_hex_nibble(unsigned char *nibble, char c)
 {
 
 	if (nibble == 0) {
+		LOG_ERROR0("Null char pointer");
 		return 3;
 	}
 	if (c >= '0' && c <= '9') {
@@ -41,6 +65,7 @@ static int from_hex_nibble(unsigned char *nibble, char c)
 	} else if (c >= 'A' && c <= 'F') {
 		*nibble = 10 + c - 'A';
 	} else {
+		LOG_ERROR1("Not hex (%c)", c);
 		return 4;
 	}
 
@@ -54,12 +79,14 @@ static int from_hex(unsigned char *byte, char high, char low)
 
 	err = from_hex_nibble(&nibble, high);
 	if (err) {
+		LOG_ERROR1("Error with high nibble (%c)", high);
 		return err;
 	}
 	*byte = (nibble << 4);
 
 	err = from_hex_nibble(&nibble, low);
 	if (err) {
+		LOG_ERROR1("Error with low nibble (%c)", high);
 		return err;
 	}
 	*byte += nibble;
@@ -73,12 +100,15 @@ int ehbi_from_hex_string(struct ehbigint *bi, const char *str, size_t str_len)
 	unsigned char high, low;
 
 	if (bi == 0) {
+		LOG_ERROR0("Null struct");
 		return 5;
 	}
 	if (str == 0) {
+		LOG_ERROR0("Null string");
 		return 6;
 	}
 	if (str_len == 0 || str[0] == 0) {
+		LOG_ERROR0("Zero length string");
 		return 7;
 	}
 
@@ -108,16 +138,17 @@ int ehbi_from_hex_string(struct ehbigint *bi, const char *str, size_t str_len)
 			high = '0';
 		}
 		if (bi->bytes_used >= bi->bytes_len) {
-			/* not enough room in bi->bytes */
+			LOG_ERROR0("byte[] too small");
 			return 8;
 		}
 		if (from_hex(&(bi->bytes[--i]), high, low)) {
+			LOG_ERROR2("Bad data (high: %c, low: %c)", high, low);
 			return 9;
 		}
 		bi->bytes_used++;
 	}
 
-	/* let's just zero out the rest of the bytes, for eazier debug */
+	/* let's just zero out the rest of the bytes, for easier debug */
 	while (i-- > 0) {
 		bi->bytes[i] = 0;
 	}
@@ -131,12 +162,15 @@ int ehbi_to_hex_string(struct ehbigint *bi, char *buf, size_t buf_len)
 	size_t i, j;
 
 	if (bi == 0) {
+		LOG_ERROR0("Null struct");
 		return 10;
 	}
 	if (buf == 0) {
+		LOG_ERROR0("Null buffer");
 		return 11;
 	}
 	if (buf_len < (bi->bytes_used + 3)) {
+		LOG_ERROR0("Buffer too small");
 		return 12;
 	}
 	j = 0;
@@ -145,15 +179,18 @@ int ehbi_to_hex_string(struct ehbigint *bi, char *buf, size_t buf_len)
 
 	for (i = bi->bytes_len - bi->bytes_used; i < bi->bytes_len; ++i) {
 		if (j + 2 > buf_len) {
+			LOG_ERROR0("Buffer too small, partially written");
 			return 13;
 		}
 		err = to_hex(bi->bytes[i], buf + j, buf + j + 1);
 		if (err) {
+			LOG_ERROR0("Corrupted data?");
 			return err;
 		}
 		j += 2;
 	}
 	if (j > buf_len) {
+		LOG_ERROR0("Unable to write trailing NULL to buffer");
 		return 14;
 	}
 	buf[j] = '\0';
@@ -168,6 +205,7 @@ int ehbi_add(struct ehbigint *res, struct ehbigint *bi1, struct ehbigint *bi2)
 	struct ehbigint *tmp;
 
 	if (res == 0 || bi1 == 0 || bi2 == 0) {
+		LOG_ERROR0("Null argument(s)");
 		return 15;
 	}
 
@@ -183,6 +221,7 @@ int ehbi_add(struct ehbigint *res, struct ehbigint *bi1, struct ehbigint *bi2)
 		b = (bi2->bytes_used < i) ? 0 : bi2->bytes[bi2->bytes_len - i];
 		c = c + a + b;
 		if (res->bytes_used + 1 > res->bytes_len) {
+			LOG_ERROR0("Result byte[] too small");
 			return 15;
 		}
 		res->bytes[res->bytes_len - i] = (unsigned char)c;
@@ -190,6 +229,10 @@ int ehbi_add(struct ehbigint *res, struct ehbigint *bi1, struct ehbigint *bi2)
 		c = c >> 8;
 	}
 	while (c) {
+		if (res->bytes_used + 1 > res->bytes_len) {
+			LOG_ERROR0("Result byte[] too small for carry");
+			return 16;
+		}
 		res->bytes[res->bytes_len - i] = (unsigned char)c;
 		res->bytes_used++;
 		c = c >> 8;

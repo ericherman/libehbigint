88102e15f98b5105c6aecdce84e27de6c071e37c
Eric Herman <eric@freesa.org>
2016-11-13 15:58:26 +0100

    refactor div test a bit

--- tests/test-div.c
+++ tests/test-div.c
@@ -14,14 +14,10 @@ License for more details.
 */
 #include "test-ehbigint-private-utils.h"
 
-int test_div(int verbose)
+int test_div(int verbose, char *snumerator, char *sdenominator, char *squotient,
+	     char *sremainder)
 {
 	int err, failures;
-
-	unsigned long ulnumerator = 287713;
-	unsigned long uldenominator = 571;
-	unsigned long uliquotient = 503;
-	unsigned long ulremainder = 500;
 
 	unsigned char bytes_numerator[10];
 	unsigned char bytes_denominator[10];
@@ -32,10 +28,6 @@ int test_div(int verbose)
 	struct ehbigint denominator;
 	struct ehbigint quotient;
 	struct ehbigint remainder;
-
-	char as_string[BUFLEN];
-	char expected[BUFLEN];
-	char *str;
 
 	VERBOSE_ANNOUNCE(verbose);
 	failures = 0;
@@ -56,33 +48,33 @@ int test_div(int verbose)
 	remainder.bytes_len = 10;
 	remainder.bytes_used = 0;
 
-	sprintf(as_string, "%lu", ulnumerator);
-	err = ehbi_set_decimal_string(&numerator, as_string, strlen(as_string));
-	if (err) {
-		LOG_ERROR1("error %d from ehbi_set_hex_string\n", err);
-		LOG_ERROR("Aborting test\n");
-		return (1 + failures);
-	}
-	failures +=
-	    check_ehbigint_dec(&numerator, as_string, __LINE__, TEST_FUNC);
-	if (failures) {
-		LOG_ERROR1("round trip failed %lu\n", ulnumerator);
-		LOG_ERROR("Aborting test\n");
-		return (1 + failures);
-	}
-
-	sprintf(as_string, "%lu", uldenominator);
 	err =
-	    ehbi_set_decimal_string(&denominator, as_string, strlen(as_string));
+	    ehbi_set_decimal_string(&numerator, snumerator, strlen(snumerator));
 	if (err) {
 		LOG_ERROR1("error %d from ehbi_set_hex_string\n", err);
 		LOG_ERROR("Aborting test\n");
 		return (1 + failures);
 	}
 	failures +=
-	    check_ehbigint_dec(&denominator, as_string, __LINE__, TEST_FUNC);
+	    check_ehbigint_dec(&numerator, snumerator, __LINE__, TEST_FUNC);
 	if (failures) {
-		LOG_ERROR1("round trip failed %lu\n", uldenominator);
+		LOG_ERROR1("round trip failed %s\n", snumerator);
+		LOG_ERROR("Aborting test\n");
+		return (1 + failures);
+	}
+
+	err =
+	    ehbi_set_decimal_string(&denominator, sdenominator,
+				    strlen(sdenominator));
+	if (err) {
+		LOG_ERROR1("error %d from ehbi_set_hex_string\n", err);
+		LOG_ERROR("Aborting test\n");
+		return (1 + failures);
+	}
+	failures +=
+	    check_ehbigint_dec(&denominator, sdenominator, __LINE__, TEST_FUNC);
+	if (failures) {
+		LOG_ERROR1("round trip failed %s\n", sdenominator);
 		LOG_ERROR("Aborting test\n");
 		return (1 + failures);
 	}
@@ -94,45 +86,11 @@ int test_div(int verbose)
 		return (1 + failures);
 	}
 
-	sprintf(expected, "%lu", uliquotient);
 	failures +=
-	    check_ehbigint_dec(&quotient, expected, __LINE__, TEST_FUNC);
-
-	sprintf(expected, "%lu", ulremainder);
-	failures +=
-	    check_ehbigint_dec(&remainder, expected, __LINE__, TEST_FUNC);
-
-	/*
-	   lldiv_t result = lldiv(5088824049625,33554393);
-	   result.quot: 151658, result.remainder: 31916031
-	 */
-	str = "5088824049625";
-	err = ehbi_set_decimal_string(&numerator, str, strlen(str));
-	if (err) {
-		LOG_ERROR3
-		    ("error %d ehbi_set_decimal_string(&numerator, %s, %lu)\n",
-		     err, str, (unsigned long)strlen(str));
-		LOG_ERROR("Aborting test\n");
-		return (1 + failures);
-	}
-	str = "33554393";
-	denominator.bytes_used = 0;
-	ehbi_set_decimal_string(&denominator, str, strlen(str));
-	err = ehbi_div(&quotient, &remainder, &numerator, &denominator);
-	if (err) {
-		LOG_ERROR1("error %d from ehbi_div\n", err);
-		LOG_ERROR("Aborting test\n");
-		return (1 + failures);
-	}
+	    check_ehbigint_dec(&quotient, squotient, __LINE__, TEST_FUNC);
 
 	failures +=
-	    check_ehbigint_dec(&quotient, "151658", __LINE__, TEST_FUNC);
-	failures +=
-	    check_ehbigint_dec(&remainder, "31916031", __LINE__, TEST_FUNC);
-
-	if (failures) {
-		LOG_ERROR1("%d failures in test_div\n", failures);
-	}
+	    check_ehbigint_dec(&remainder, sremainder, __LINE__, TEST_FUNC);
 
 	return failures;
 }
@@ -144,7 +102,14 @@ int main(int argc, char **argv)
 	v = (argc > 1) ? atoi(argv[1]) : 0;
 	failures = 0;
 
-	failures += test_div(v);
+	failures += test_div(v, "287713", "571", "503", "500");
+
+	/*
+	   lldiv_t result = lldiv(5088824049625,33554393);
+	   result.quot: 151658, result.remainder: 31916031
+	 */
+	failures +=
+	    test_div(v, "5088824049625", "33554393", "151658", "31916031");
 
 	if (failures) {
 		LOG_ERROR2("%d failures in %s\n", failures, __FILE__);

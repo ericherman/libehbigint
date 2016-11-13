eb45208b7392113e1a856cb11399284b06ab70f1
Eric Herman <eric@freesa.org>
2016-11-13 15:40:13 +0100

    demos/bi-calc.c: support mulitiplication, division, remainder

--- demos/bi-calc.c
+++ demos/bi-calc.c
@@ -22,12 +22,14 @@ int main(int argc, char *argv[])
 	unsigned char bbuf1[1024];
 	unsigned char bbuf2[1024];
 	unsigned char bbuf3[1024];
+	unsigned char bbuf4[1024];
 	struct ehbigint bigint_1;
 	struct ehbigint bigint_2;
 	struct ehbigint bigint_3;
+	struct ehbigint bigint_4;
 
 	if (argc < 4) {
-		fprintf(stderr, "usage:\n\t%s decimal1 [+-] decimal2\n",
+		fprintf(stderr, "usage:\n\t%s decimal1 [+-*/%%] decimal2\n",
 			argv[0]);
 		return -1;
 	}
@@ -41,6 +43,9 @@ int main(int argc, char *argv[])
 	bigint_3.bytes = bbuf3;
 	bigint_3.bytes_len = 1024;
 
+	bigint_4.bytes = bbuf4;
+	bigint_4.bytes_len = 1024;
+
 	ehbi_set_decimal_string(&bigint_1, argv[1], strlen(argv[1]));
 	ehbi_set_decimal_string(&bigint_2, argv[3], strlen(argv[3]));
 
@@ -50,6 +55,15 @@ int main(int argc, char *argv[])
 		break;
 	case '-':
 		ehbi_subtract(&bigint_3, &bigint_1, &bigint_2);
+		break;
+	case '*':
+		ehbi_mul(&bigint_3, &bigint_1, &bigint_2);
+		break;
+	case '/':
+		ehbi_div(&bigint_3, &bigint_4, &bigint_1, &bigint_2);
+		break;
+	case '%':
+		ehbi_div(&bigint_4, &bigint_3, &bigint_1, &bigint_2);
 		break;
 	default:
 		fprintf(stderr, "operator '%c' not supported.\n", *argv[2]);

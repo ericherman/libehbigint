dde8f1720b38e2f4794d84b5a6762fc02eab0261
Eric Herman <eric@freesa.org>
2019-08-28 14:01:37 +0200

    SPDX-License-Identifier: LGPL-3.0-or-later

--- src/ehbigint-arduino.h
+++ src/ehbigint-arduino.h
@@ -1,19 +1,6 @@
-/*
- * Copyright (C) 2012 Eric Herman <eric@freesa.org>
- *
- * This library is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with this library.  If not, see <http://www.gnu.org/licenses/>.
- */
+/* SPDX-License-Identifier: LGPL-3.0-or-later */
+/* ehbigint-arduino.h: a smallish-footprint bigint suitable for arduino */
+/* Copyright (C) 2012, 2019 Eric Herman <eric@freesa.org> */
 
 #ifndef EHBIGINT_ARDUINO_H
 #define EHBIGINT_ARDUINO_H
